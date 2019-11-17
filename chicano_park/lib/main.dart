d6ba404a452dbc69f457a8d0f5b006d97b4873dc
Rajan Phadnis <rajansd28@gmail.com>
2019-11-17 13:47:42 -0800

    added fab setstate and icon options

--- chicano_park/lib/main.dart
+++ chicano_park/lib/main.dart
@@ -4,12 +4,10 @@ import 'package:camera/camera.dart';
 
 List<CameraDescription> cameras;
 // set app starting page and then run app
-Future<void> main() async {
-  cameras = await availableCameras();
-  runApp(MyApp());
-}
-// void main() => runApp(MyApp());
 
+// void main() => runApp(MyApp());
+void logError(String code, String message) =>
+    print('Error: $code\nError Message: $message');
 // Create the main page element
 class MyApp extends StatelessWidget {
   // build the main page element (render)
@@ -29,7 +27,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    logError(e.code, e.description);
+  }
+  runApp(MyApp());
+}
 // Create a "redirect" element
 class MainHomePage extends StatefulWidget {
   MainHomePage({Key key, this.title}) : super(key: key);
@@ -38,10 +45,100 @@ class MainHomePage extends StatefulWidget {
   _MainHomePageState createState() => _MainHomePageState();
 }
 
+class FabFunctionToCallJustToCleanThingsUpAndMakeItEasierToSeeStuff
+    extends StatefulWidget {
+  _FabFunctionToCallJustToCleanThingsUpAndMakeItEasierToSeeStuff
+      createState() =>
+          _FabFunctionToCallJustToCleanThingsUpAndMakeItEasierToSeeStuff();
+}
+
+class _FabFunctionToCallJustToCleanThingsUpAndMakeItEasierToSeeStuff
+    extends State<
+        FabFunctionToCallJustToCleanThingsUpAndMakeItEasierToSeeStuff> {
+  bool showFab = true;
+  @override
+  Widget build(BuildContext context) {
+    return showFab
+        ? FloatingActionButton(
+            onPressed: () {
+              var bottomSheetController = showBottomSheet(
+                  context: context,
+                  builder: (context) => Container(
+                        color: Colors.transparent,
+                        width: double.infinity,
+                        height: MediaQuery.of(context).size.height * 0.9,
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: new BorderRadius.only(
+                                topLeft: new Radius.circular(10),
+                                topRight: new Radius.circular(10),
+                              ),
+                              color: Colors.grey),
+                          child: Column(
+                            children: <Widget>[
+                              new Padding(
+                                padding: const EdgeInsets.all(10),
+                                child: new CustomPaint(
+                                  painter:
+                                      PaintSomeRandomShapeThatIsProbablyARectangleWithSomeRadius(),
+                                  child: Container(
+                                    decoration: BoxDecoration(
+                                        border:
+                                            new Border.all(color: Colors.grey),
+                                        borderRadius: new BorderRadius.all(
+                                            Radius.circular(2.5)),
+                                        color: Colors.white),
+                                    height: 5,
+                                    width: 40,
+                                  ),
+                                ),
+                              ),
+                              new Padding(
+                                padding: const EdgeInsets.all(30.0),
+                                child: Text(
+                                  'Drag to dismiss',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 24.0,
+                                    fontFamily: "Baskerville",
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ));
+              showFAB(false);
+              bottomSheetController.closed.then((value) {
+                showFAB(true);
+              });
+            },
+            // child: Icon(Icons.apps),
+            // child: Icon(Icons.art_track),
+            // child: Icon(Icons.assistant),
+            // child: Icon(Icons.burst_mode),
+            // child: Icon(Icons.camera_enhance),
+            // child: Icon(Icons.dashboard),
+            // child: Icon(Icons.dns),
+            // child: Icon(Icons.filter_list),
+            child: Icon(Icons.explore),
+            // child: Icon(Icons.search),
+          )
+        : Container();
+  }
+
+  void showFAB(bool value) {
+    setState(() {
+      showFab = value;
+    });
+  }
+}
+
 class _MainHomePageState extends State<MainHomePage> {
   // setup basic variables
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   CameraController controller;
+  bool showFab = true;
   // set app behavior when app initializes from cold start
   @override
   void initState() {
@@ -62,50 +159,6 @@ class _MainHomePageState extends State<MainHomePage> {
     super.dispose();
   }
 
-  void _showBottomSheetCallback() {
-    _scaffoldKey.currentState.showBottomSheet<void>((BuildContext context) {
-      return Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height * 0.9,
-          decoration: BoxDecoration(
-              // border: Border(top: BorderSide(color: Colors.black)),
-              borderRadius: new BorderRadius.only(
-                topLeft: new Radius.circular(10),
-                topRight: new Radius.circular(10),
-              ),
-              color: Colors.grey),
-          child: Column(
-            children: <Widget>[
-              new Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: new CustomPaint(
-                    painter:
-                        PaintSomeRandomShapeThatIsProbablyARectangleWithSomeRadius(),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: new Border.all(color: Colors.grey),
-                          borderRadius:
-                              new BorderRadius.all(Radius.circular(2.5)),
-                          color: Colors.white),
-                      height: 5,
-                      width: 40,
-                    ),
-                  )),
-              new Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Text(
-                  'Drag to dismiss',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24.0,
-                  ),
-                ),
-              ),
-            ],
-          ));
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -113,13 +166,17 @@ class _MainHomePageState extends State<MainHomePage> {
         key: _scaffoldKey,
         body: _cameraPreviewWidget(),
         // Get rid of fab onpress: https://medium.com/flutter-community/flutter-beginners-guide-to-using-the-bottom-sheet-b8025573c433
-        floatingActionButton: FloatingActionButton(
-          onPressed: _showBottomSheetCallback,
-          child: Icon(Icons.navigation),
-          backgroundColor: Colors.green,
-        ),
+        floatingActionButton:
+            FabFunctionToCallJustToCleanThingsUpAndMakeItEasierToSeeStuff(),
       ),
     );
+    // )
+  }
+
+  void showFAB(bool value) {
+    setState(() {
+      showFab = value;
+    });
   }
 
   Widget _cameraPreviewWidget() {
