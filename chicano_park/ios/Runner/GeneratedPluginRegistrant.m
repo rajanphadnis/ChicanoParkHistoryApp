ff4a24a735d223454ac9a3c4c82ab8dbe93cb631
Rajan Phadnis <rajansd28@gmail.com>
2019-12-14 19:38:50 -0800

    works now!!!

--- chicano_park/ios/Runner/GeneratedPluginRegistrant.m
+++ chicano_park/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,42 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
-
-#if __has_include(<camera/CameraPlugin.h>)
 #import <camera/CameraPlugin.h>
-#else
-@import camera;
-#endif
-
-#if __has_include(<cloud_firestore/FLTCloudFirestorePlugin.h>)
-#import <cloud_firestore/FLTCloudFirestorePlugin.h>
-#else
-@import cloud_firestore;
-#endif
-
-#if __has_include(<firebase_core/FLTFirebaseCorePlugin.h>)
-#import <firebase_core/FLTFirebaseCorePlugin.h>
-#else
-@import firebase_core;
-#endif
-
-#if __has_include(<firebase_livestream_ml_vision/FirebaseLivestreamMlVisionPlugin.h>)
+#import <cloud_firestore/CloudFirestorePlugin.h>
+#import <firebase_core/FirebaseCorePlugin.h>
 #import <firebase_livestream_ml_vision/FirebaseLivestreamMlVisionPlugin.h>
-#else
-@import firebase_livestream_ml_vision;
-#endif
-
-#if __has_include(<firebase_storage/FLTFirebaseStoragePlugin.h>)
-#import <firebase_storage/FLTFirebaseStoragePlugin.h>
-#else
-@import firebase_storage;
-#endif
-
-#if __has_include(<share/FLTSharePlugin.h>)
-#import <share/FLTSharePlugin.h>
-#else
-@import share;
-#endif
+#import <firebase_storage/FirebaseStoragePlugin.h>
+#import <share/SharePlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
