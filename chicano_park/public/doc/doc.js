c71be6bfb6bc1737aee9cff78bfd2be57ef72c74
Rajan Phadnis <rajansd28@gmail.com>
2020-02-13 12:02:56 -0800

    added real firebase auth

--- chicano_park/public/doc/doc.js
+++ chicano_park/public/doc/doc.js
@@ -7,14 +7,24 @@ function userShow() {
 function scShow() {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
-          // User is signed in.
-          document.getElementById("message").style.display = "none";
+          if (user.email == "rphadnis20@pacificridge.org" || user.email == "rajansd28@gmail.com") {
+            document.getElementById("message").style.display = "none";
           document.getElementById("scDoc").style.display = "block";
           document.getElementById("userDoc").style.display = "none";
           document.getElementById("loginE").style.display = "none";
           document.getElementById("sc").innerHTML = "Welcome, " + user.displayName;
+          }
+          else {
+            document.getElementById("scDoc").style.display = "none";
+            document.getElementById("userDoc").style.display = "none";
+            document.getElementById("loginE").style.display = "block";
+            document.getElementById("message").style.display = "none";
+            document.getElementById("loginE").innerHTML = "Sorry, you don't have access. <a onclick='logout()' href=''>Logout</a>";
+          }
         } else {
           // No user is signed in.
+          document.getElementById("scDoc").style.display = "none";
+          document.getElementById("userDoc").style.display = "none";
           document.getElementById("loginE").style.display = "block";
           document.getElementById("message").style.display = "none";
         }
