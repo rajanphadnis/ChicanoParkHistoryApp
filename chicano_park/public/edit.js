33a70b3fbdf595b7192ee2ae3990be07919bdbd7
Rajan Phadnis <rajansd28@gmail.com>
2020-02-13 12:22:01 -0800

    added real auth

--- chicano_park/public/edit.js
+++ chicano_park/public/edit.js
@@ -1,14 +1,14 @@
-var config = {
-    apiKey: "AIzaSyC1IMIc5fVsAF30_pi08gc03pz94CX2Avk",
-    authDomain: "programmingii-367d0.firebaseapp.com",
-    databaseURL: "https://programmingii-367d0.firebaseio.com",
-    projectId: "programmingii-367d0",
-    storageBucket: "programmingii-367d0.appspot.com",
-    messagingSenderId: "374441578679",
-    appId: "1:374441578679:web:33e2088b751f0169a77d32",
-    measurementId: "G-1GTHN2PHCX"
-};
-firebase.initializeApp(config);
+// var config = {
+//     apiKey: "AIzaSyC1IMIc5fVsAF30_pi08gc03pz94CX2Avk",
+//     authDomain: "programmingii-367d0.firebaseapp.com",
+//     databaseURL: "https://programmingii-367d0.firebaseio.com",
+//     projectId: "programmingii-367d0",
+//     storageBucket: "programmingii-367d0.appspot.com",
+//     messagingSenderId: "374441578679",
+//     appId: "1:374441578679:web:33e2088b751f0169a77d32",
+//     measurementId: "G-1GTHN2PHCX"
+// };
+// firebase.initializeApp(config);
 var firestore = firebase.firestore();
 var selected = true;
 
@@ -26,16 +26,35 @@ firebase.firestore().enablePersistence({
             // ...
         }
     });
-
+firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+        if (user.email == "rphadnis20@pacificridge.org" || user.email == "rajansd28@gmail.com") {
+            document.getElementById("murals").style.display = "block";
+            document.getElementById("artists").style.display = "block";
+            document.getElementById("noAuth").style.display = "none";
+            document.getElementById("noAur").style.display = "none";
+        } else {
+            document.getElementById("murals").style.display = "none";
+            document.getElementById("artists").style.display = "none";
+            document.getElementById("noAuth").style.display = "none";
+            document.getElementById("noAur").style.display = "block";
+        }
+    } else {
+        document.getElementById("murals").style.display = "none";
+        document.getElementById("artists").style.display = "none";
+        document.getElementById("noAuth").style.display = "block";
+        document.getElementById("noAur").style.display = "none";
+    }
+});
 var StringThing = '<h1>Murals</h1>';
 firestore.collection("Murals/").get().then(function (querySnapshot) {
     querySnapshot.forEach(function (doc) {
-        StringThing = StringThing + '<div class="cardDiv mdc-card"><input disabled class="centerTheThing" value="' + doc.id + '"><p>Title:</p><input class="VT" value="' + doc.data().title + 
-        '"></br><p>Picture:</p><input class="VP" value="' + doc.data().picURL + 
-        '"></br><p>Description:</p><textarea rows="4" class="VD" wrap="soft">' + doc.data().desc +
-        '</textarea></br><p>Author:</p><input class="VA" value="' + doc.data().author +'"></div>';
+        StringThing = StringThing + '<div class="cardDiv mdc-card"><input disabled class="centerTheThing" value="' + doc.id + '"><p>Title:</p><input class="VT" value="' + doc.data().title +
+            '"></br><p>Picture:</p><input class="VP" value="' + doc.data().picURL +
+            '"></br><p>Description:</p><textarea rows="4" class="VD" wrap="soft">' + doc.data().desc +
+            '</textarea></br><p>Author:</p><input class="VA" value="' + doc.data().author + '"></div>';
         document.getElementById("murals").innerHTML = StringThing;
-        
+
     });
 }).catch(function (error) {
     console.log("Error getting document:", error);
@@ -46,11 +65,11 @@ document.getElementById("murals").innerHTML = StringThing;
 var ArtistString = '<h1>Artists</h1>';
 firestore.collection("Artists/").get().then(function (querySnapshot) {
     querySnapshot.forEach(function (doc) {
-        ArtistString = ArtistString + '<div class="cardDiv mdc-card"><input disabled class="centerArt" value="' + doc.id + '"><p>Picture:</p><input class="ArtistPic" value="' + doc.data().picURL + 
-        '"></br><p>Description:</p><textarea rows="4" class="ArtistDesc" wrap="soft">' + doc.data().desc +
-        '</textarea></div>';
+        ArtistString = ArtistString + '<div class="cardDiv mdc-card"><input disabled class="centerArt" value="' + doc.id + '"><p>Picture:</p><input class="ArtistPic" value="' + doc.data().picURL +
+            '"></br><p>Description:</p><textarea rows="4" class="ArtistDesc" wrap="soft">' + doc.data().desc +
+            '</textarea></div>';
         document.getElementById("artists").innerHTML = ArtistString;
-        
+
     });
 }).catch(function (error) {
     console.log("Error getting document:", error);
@@ -68,14 +87,16 @@ document.getElementById("artSave").addEventListener("click", function () {
     updateA(AT);
     alert("Updated data. Refresh page to see changes.");
 });
+
 function openCity(cityName) {
     var i;
     var x = document.getElementsByClassName("tabbar");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";  
+        x[i].style.display = "none";
     }
-    document.getElementById(cityName).style.display = "block";  
-  }
+    document.getElementById(cityName).style.display = "block";
+}
+
 function updateV(lengthOfV) {
     console.log(lengthOfV);
     var i;
@@ -97,6 +118,7 @@ function updateV(lengthOfV) {
             });
     }
 }
+
 function updateA(lengthOfA) {
     console.log(lengthOfA);
     var i;
